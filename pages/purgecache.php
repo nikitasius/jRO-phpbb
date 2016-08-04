98b251476c2ec3670c12e9720e2c771a37a5edcc
nikitasius <nikita@saraeff.net>
2016-08-04 16:52:45 +0200

    fix `*` for GPLv3

--- pages/purgecache.php
+++ pages/purgecache.php
@@ -1,22 +1,22 @@
 <?php
 /**
  * purgecache.php - purging cache for phpbb forum
-    Copyright (C) 2016  Nikita S. <nikita@saraeff.net>
-
-    This file is part of jRO-phpbb.
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *  Copyright (C) 2016  Nikita S. <nikita@saraeff.net>
+ *
+ *  This file is part of jRO-phpbb.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 define('IN_PHPBB', true);
 $phpbb_root_path = './';  // Here write relative path to your phpBB installation directory, default is '/'
